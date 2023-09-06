bc9bcf50463400c909507f553b5a9e4a8690c09e
OscarG <oscar.gamez88@gmail.com>
2023-09-06 11:12:29 -0700

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,12 @@ def list_projects(request):
         "list_projects": list_projects,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    show_project = Project.objects.get(id=id)
+    context = {
+        "show_project": show_project,
+    }
+    return render(request, "projects/detail.html", context)
